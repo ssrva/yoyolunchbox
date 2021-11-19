1fc683cd3bf89a5a5828550cb97f8d2950618ea5
Srivathsan <sri@Srivathsans-MacBook-Air.local>
2021-11-19 22:50:22 +0530

    Add necessary files

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
 import 'expo-asset';
 import { AppRegistry } from 'react-native';
+import { registerRootComponent } from 'expo';
 
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-// registerRootComponent(App);
-AppRegistry.registerComponent('main', () => App);
+registerRootComponent(App);
+// AppRegistry.registerComponent('main', () => App);
